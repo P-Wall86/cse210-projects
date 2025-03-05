14c32ead5733682681dc54ed257dedfa2e3c7d8d
P-Wall86 <sisterwall@gmail.com>
2025-03-05 15:37:12 -0300

    Finished 4th Exercise. No stretches yet.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine();
+
+    List<int> numbers = new List<int>(); 
+    double sum = 0;
+    double average = 0;
+    int largest = 0;
+
+    while (true) 
+    {
+        Console.Write("Please insert a number of your choice. Type 0 to exit. ");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+
+        if (number != 0)
+        {
+            numbers.Add(number);
+            sum += number;
+            Console.WriteLine("Number added to the list.");
+
+             if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        else 
+        {
+            if (numbers.Count > 0)
+            {
+                average = sum / numbers.Count;
+            }
+
+            Console.WriteLine($"The sum is {sum};");
+            Console.WriteLine($"The average is {average}.");
+            Console.WriteLine($"The largest number is {largest}.");
+            Console.WriteLine("Program terminated.");
+            break;
+        }
+    }
     }
 }
