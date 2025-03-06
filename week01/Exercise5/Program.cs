3fe63ab638e345b0d12fd57b89a05fc3c9c50771
P-Wall86 <sisterwall@gmail.com>
2025-03-06 08:39:00 -0300

    Finished 5th Exercise. No stretches required in this one.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine();
+        DisplayWelcome();
+        Console.WriteLine();
+        
+        string name = PromptUserName();
+
+        int favnum = PromptUserNumber();
+
+        int square = SquareNumber(favnum);
+        DisplayResult(name, square);
     }
-}
+    
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to CSE 210!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name?: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number?: ");
+        int favnum = int.Parse(Console.ReadLine());
+        return favnum;
+    }
+
+    static int SquareNumber(int favnum)
+    {
+        return favnum * favnum;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, your favorite number squared is {square}.");
+    }
+    }
