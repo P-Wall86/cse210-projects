61bf2c1378c17c8d25a108f54e948b006824c0e3
P-Wall86 <sisterwall@gmail.com>
2025-03-15 16:42:27 -0300

    Grammar

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
-/*I decided to save the entries in a CSV file. I had to make some research about many things
+/*I decided to save the entries in a CSV file. I had to do my research about things
 I didn't understand: how to use StreamWriter and StreamReader, even though I used just one of them in the Journal.cs file.*/
 
 class Program
