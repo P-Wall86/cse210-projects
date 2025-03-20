6d42da1ec329ab66707ea5012d7900e31298634c
P-Wall86 <sisterwall@gmail.com>
2025-03-19 22:05:21 -0300

    Fractions Assignment.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"Fraction: {fraction1.GetFractionString()}");
+        Console.WriteLine($"Decimal: {fraction1.GetDecimalValue()}");
+        Console.WriteLine();
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine($"Fraction: {fraction2.GetFractionString()}");
+        Console.WriteLine($"Decimal: {fraction2.GetDecimalValue()}"); 
+        Console.WriteLine();
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine($"Fraction: {fraction3.GetFractionString()}");
+        Console.WriteLine($"Decimal: {fraction3.GetDecimalValue()}"); 
+        Console.WriteLine();
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine($"Fraction: {fraction4.GetFractionString()}");
+        Console.WriteLine($"Decimal: {fraction4.GetDecimalValue()}");
     }
 }
