a4bc9a81f0512356b3f4279c1d13cca0507a406c
P-Wall86 <sisterwall@gmail.com>
2025-03-31 21:16:13 -0300

    Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.WriteLine();
+
+        MathAssignment mathHomework = new MathAssignment("Noah Smith", "Algebra", "5.2", "1-10");  
+        Console.WriteLine(mathHomework.GetSummary());
+        Console.WriteLine(mathHomework.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writingHomework = new WritingAssignment("Oliver Cowdery", "Writing 101", "My experience with the American indians.");  
+        Console.WriteLine(writingHomework.GetSummary());
+        Console.WriteLine(writingHomework.GetWritingInformation());
     }
 }
